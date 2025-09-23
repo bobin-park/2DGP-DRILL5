603a4f625f85f82a3f493a50209f3ac0c155ff79
Park bobin <asapdodin@tukorea.ac.kr>
2025-09-23 11:45:20 +0900

    좌우 이동 오류 해결 코드

--- character_runs_esc.py
+++ character_runs_esc.py
@@ -5,14 +5,12 @@ grass = load_image('grass.png')
 character = load_image('animation_sheet.png')
 
 # fill here
-running = True
-x=800//2
+
 frame =0
 
 def handle_events():
     # fill here
-    global running
-    global x
+    global running , dir
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT: #window의 x버튼
@@ -21,11 +19,19 @@ def handle_events():
             if event.key == SDLK_ESCAPE: #esc키를 눌렀을 떄
                running = False
             elif event.key ==SDLK_LEFT:
-                x-=10
+                dir-=1
             elif event.key == SDLK_RIGHT:
-                x+=10
+                dir+=1
+        elif event.type == SDL_KEYUP:
+            if event.key == SDLK_LEFT:
+                dir+=1
+            elif event.key == SDLK_RIGHT:
+                dir-=1
     pass
 
+running = True
+x=800//2
+dir=0
 
 while running:
     clear_canvas()
@@ -34,6 +40,7 @@ while running:
     update_canvas()
     handle_events()
     frame = (frame + 1) % 8
+    x+=dir*5
     delay(0.05)
 
 
