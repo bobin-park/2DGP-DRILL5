e39902f38554d6113201e6a85717c2fc5518da42
Park bobin <asapdodin@tukorea.ac.kr>
2025-09-23 11:29:01 +0900

    events 동작 코드

--- character_runs_esc.py
+++ character_runs_esc.py
@@ -4,15 +4,19 @@ open_canvas()
 grass = load_image('grass.png')
 character = load_image('animation_sheet.png')
 
-
 # fill here
-
-
+running = True
 
 def handle_events():
     # fill here
+    global running
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT: #window의 x버튼
+            running = False
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE: #esc키를 눌렀을 떄
+            running = False
     pass
-
 
 frame = 0
 for x in range(0, 800, 5):
@@ -23,8 +27,9 @@ for x in range(0, 800, 5):
     update_canvas()
 
     # fill here
-
-
+    handle_events()
+    if not running:
+        break;
 
     frame = (frame + 1) % 8
     delay(0.05)
