15c32adb3753abcf74772438d630c8636acd9892
Park bobin <asapdodin@tukorea.ac.kr>
2025-09-23 11:33:46 +0900

    esc키 미작동 오류 해설

--- character_runs_esc.py
+++ character_runs_esc.py
@@ -9,11 +9,13 @@ running = True
 
 def handle_events():
     # fill here
-    global running
+    # global running  # 함수 내 사용되는 지역 변수가 아닌 전역 변수임을 명시함
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT: #window의 x버튼
             running = False
+            #running가 전역변수가 아닌 지역변수로 인식됨
+            # 즉, esc를 눌렀을 떄 running 라는 지역 변수가 생성되는 것으로 인식
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE: #esc키를 눌렀을 떄
             running = False
     pass
