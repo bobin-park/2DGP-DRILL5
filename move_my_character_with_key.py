b4150229ebd82a4a66d353562d8966f77cfd849f
Park bobin <asapdodin@tukorea.ac.kr>
2025-10-14 16:40:51 +0900

    상하좌우 키보드 작동/캐릭터 상하좌우 이동 확인

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -6,26 +6,37 @@ open_canvas(TUK_WIDTH,TUK_HEIGHT)
 tuk_ground = load_image('TUK_GROUND.png')
 character = load_image('animation_sheet.png')
 
-x, y = 500, 500
+x, y = TUK_WIDTH//2,TUK_HEIGHT//2
+running = False
+frame=0
 
 def handle_events():
     global x,y
+    global running
     events=get_events()
     for event in events:
         if event.type == SDL_KEYDOWN and event.key  ==SDLK_UP:
             y+=10
+            running = True
         elif event.type == SDL_KEYDOWN and event.key  ==SDLK_DOWN:
             y-=10
+            running = True
         elif event.type == SDL_KEYDOWN and event.key  ==SDLK_LEFT:
             x-=10
+            running = True
         elif event.type == SDL_KEYDOWN and event.key  ==SDLK_RIGHT:
             x+=10
+            running = True
 
 
 
 while 1:
+    # global frame
     clear_canvas()
     tuk_ground.draw(TUK_WIDTH//2,TUK_HEIGHT//2)
-    character.clip_draw( 100, 100 , 100, 100, x,y)
+    if running:
+        frame+1
+    character.clip_draw( frame*100,100*1,100, 100, x,y)
     update_canvas()
     handle_events()
+    frame = (frame + 1) % 8
