2e36d67e08f9511b93b75447f9ffd15f8da7fe77
Park bobin <asapdodin@tukorea.ac.kr>
2025-10-14 16:56:38 +0900

    계속 이동하도록 수정

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -10,34 +10,31 @@ x, y = TUK_WIDTH//2,TUK_HEIGHT//2
 running = True
 frame=0
 
+dir_x, dir_y = 0, 0
 def handle_events():
-    global x,y
-    global running
-    events=get_events()
+    global running, dir_x, dir_y
+    events = get_events()
     for event in events:
-        if event.type == SDL_KEYDOWN and event.key  ==SDLK_UP:
-            y+=10
-            running = True
-        elif event.type == SDL_KEYDOWN and event.key  ==SDLK_DOWN:
-            y-=10
-            running = True
-        elif event.type == SDL_KEYDOWN and event.key  ==SDLK_LEFT:
-            x-=10
-            running = True
-        elif event.type == SDL_KEYDOWN and event.key  ==SDLK_RIGHT:
-            x+=10
-            running = True
-        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
-            exit()
-
-
+        if event.type == SDL_KEYDOWN:
+            if event.key == SDLK_UP:
+                dir_y += 1
+            elif event.key == SDLK_DOWN:
+                dir_y -= 1
+            elif event.key == SDLK_LEFT:
+                dir_x -= 1
+            elif event.key == SDLK_RIGHT:
+                dir_x += 1
+            elif event.key == SDLK_ESCAPE:
+                exit()
+        elif event.type == SDL_KEYUP:
+            dir_x = 0
+            dir_y = 0
 
 while running:
-    # global frame
     clear_canvas()
     tuk_ground.draw(TUK_WIDTH//2,TUK_HEIGHT//2)
-    if running:
-        frame+1
+    x += dir_x * 5
+    y += dir_y * 5
     character.clip_draw( frame*100,100*1,100, 100, x,y)
     update_canvas()
     handle_events()
